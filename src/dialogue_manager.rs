f88b0d4a123ab811ae55e87a2a2dc28640705514
Leonardo Kaftal <leonardokaftal1@gmail.com>
2024-05-02 22:25:24 +0200

    Removed title

--- src/dialogue_manager.rs
+++ src/dialogue_manager.rs
@@ -1,9 +1,7 @@
 use std::io::{stdin, stdout, Write};
 use crossterm::execute;
-use crossterm::cursor::{MoveToColumn, SavePosition};
 use crossterm::style::{Colors, Print, SetColors};
 use crossterm::style::Color::{Black, Red, White};
-use crossterm::terminal::size;
 use crate::game_manager::{Game, Games};
 use crate::user_config_manager::{find_game_mods_paths_in_user_config_file, save_directories_in_config_file};
 
@@ -15,34 +13,6 @@ pub fn print_red_string(text: &str) {
         Print(format!("{}\n",text)),
         SetColors(Colors::new(White, Black))
     ).unwrap();
-}
-
-
-
-pub fn print_title() {
-    let title = "TOTAL WAR MOD MANAGER";
-    let terminal_width = size().unwrap().1 * 10 + (size().unwrap().1 / 2);
-    let title_len = (title.len() * 2) as u16;
-    let padding = (terminal_width - title_len) / 2;
-
-    execute!(
-        stdout(),
-        SavePosition,
-        SetColors(Colors::new(Red, Black)),
-        MoveToColumn(padding),
-        Print(format!("{}\n", title)),
-        SetColors(Colors::new(White, Black))
-    ).expect("Impossible");
-
-
-
-    println!("ATTENTION, the mod manager will enable every mod it find in the data directory of the game, \
-    so take out of the data folder every mod you would not like to enable");
-    println!();
-    println!("ATTENTION, every mod files name should be in lowercase, the mod manager in the process of applying the mod will \
-    rename all of the mods to lowercase if found some in uppercase");
-    println!();
-    println!("ATTENTION, the mod manager only support .pack mod files, if it find some .BIN files (outdated mod format) then it will automatically convert to .pack file");
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,11 @@ mod user_config_manager;
 mod dialogue_manager;
 mod game_manager;
 
-use crate::dialogue_manager::{ask_for_game_directories_dialogue, ask_user_what_games, print_title};
+use crate::dialogue_manager::{ask_for_game_directories_dialogue, ask_user_what_games};
 use crate::game_manager::map_enum_to_game;
 
 
 fn main() {
-    print_title();
     let choose_game_enum = ask_user_what_games();
     let mut game = map_enum_to_game(&choose_game_enum, None,None);
     // there are two directories, one is the data directory that contains the .pack mods file and the other one is the user script which list all the activated mod
